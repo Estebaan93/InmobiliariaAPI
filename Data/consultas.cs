ae677cab62a0c6ef82bac85bcf4d7be0e4ec5ebd
Estebaan93 <94663767+Estebaan93@users.noreply.github.com>
2025-10-20 18:12:43 -0300

    conf

---
+++ Data/consultas.cs
@@ -0,0 +1,122 @@
+// Zed settings
+//
+// For information on how to configure Zed, see the Zed
+// documentation: https://zed.dev/docs/configuring-zed
+//
+// To see all of Zed's default settings without changing your
+// custom settings, run `zed: open default settings` from the
+// command palette (cmd-shift-p / ctrl-shift-p)
+{
+  "format_on_save": "off",
+  "pane_split_direction_horizontal": "down",
+  "drop_target_size": 0.20000004768371582,
+  "inlay_hints": {
+    "show_background": false,
+    "enabled": false
+  },
+  "show_whitespaces": "selection",
+  "use_on_type_format": false,
+  "remove_trailing_whitespace_on_save": true,
+  "auto_indent_on_paste": false,
+  "auto_indent": true,
+  "indent_guides": {
+    "background_coloring": "disabled",
+    "enabled": true
+  },
+  "hard_tabs": false,
+  "git_panel": {
+    "button": false,
+    "sort_by_path": true
+  },
+  "show_wrap_guides": true,
+  "preferred_line_length": 80,
+  "tab_size": 2,
+  "toolbar": {
+    "agent_review": true,
+    "selections_menu": true,
+    "quick_actions": false,
+    "breadcrumbs": false
+  },
+  "scrollbar": {
+    "show": "auto"
+  },
+  "gutter": {
+    "min_line_number_digits": 2
+  },
+  "helix_mode": false,
+  "vim_mode": false,
+  "zoomed_padding": true,
+  "active_pane_modifiers": {
+    "inactive_opacity": 1.0
+  },
+  "use_system_window_tabs": false,
+  "centered_layout": {
+    "right_padding": 0.19999998807907104,
+    "left_padding": 0.20000004768371582
+  },
+  "bottom_dock_layout": "contained",
+  "preview_tabs": {
+    "enabled": true
+  },
+  "tabs": {
+    "show_diagnostics": "off",
+    "show_close_button": "hover"
+  },
+  "tab_bar": {
+    "show_nav_history_buttons": false,
+    "show": true
+  },
+  "title_bar": {
+    "show_branch_icon": false,
+    "show_menus": false,
+    "show_sign_in": false,
+    "show_user_picture": true,
+    "show_onboarding_banner": true,
+    "show_project_items": false,
+    "show_branch_name": false
+  },
+  "debugger": {
+    "button": true
+  },
+  "search": {
+    "button": true
+  },
+  "diagnostics": {
+    "inline": {
+      "padding": 2
+    },
+    "button": true
+  },
+  "terminal": {
+    "button": true
+  },
+  "status_bar": {
+    "cursor_position_button": true,
+    "active_language_button": true
+  },
+  "project_panel": {
+    "indent_size": 20.0,
+    "button": true
+  },
+  "minimap": {
+    "current_line_highlight": "all",
+    "thumb": "always",
+    "display_in": "active_editor",
+    "max_width_columns": 66,
+    "show": "never"
+  },
+  "buffer_font_family": ".ZedMono",
+  "agent_ui_font_size": 15.0,
+  "ui_font_weight": 400.0,
+  "telemetry": {
+    "diagnostics": false,
+    "metrics": false
+  },
+  "ui_font_size": 15.0,
+  "buffer_font_size": 13.0,
+  "theme": {
+    "mode": "system",
+    "light": "One Light",
+    "dark": "One Dark"
+  }
+}
